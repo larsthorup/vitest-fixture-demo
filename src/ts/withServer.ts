4c5a6431339910b8f447d0c4eab40681ee264a69
Lars Thorup <lars@zealake.com>
2022-07-11 18:40:42 +0200

    types

---
+++ src/ts/withServer.ts
@@ -0,0 +1,12 @@
+import { test as base } from "vitest-fixture";
+
+type Server = { port: number };
+export const test = base.extend<{ port: number; server: Server | undefined }>({
+  port: 8000,
+  server: async ({ port }, use) => {
+    let server: Server | undefined = { port };
+    await use(server, async () => {
+      server = undefined;
+    });
+  },
+});
